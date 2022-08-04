0b22ede10eca2824809dafa4dc14e6adc501b86b
Caleb Craigo <CalebDOS1@yahoo.com>
2022-08-04 00:11:27 -0400

    Item tag

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -119,7 +119,7 @@ const Body = () => {
                                 <Item>xs=8</Item>
                             </Grid>
                             <Grid item xs={4}>
-                                Item>xs=4</Item>
+                                <Item>xs=4</Item>
                             </Grid>
                                 <Grid item xs={4}>
                             <Item>xs=4</Item>
